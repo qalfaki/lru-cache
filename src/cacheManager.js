638be84e087cb5d3439e4417891251011ae582d1
syntaxsizer <qusai.alfaki@gmail.com>
2019-10-29 00:32:10 +0300

    a helper class that will publish cache changes to other distributed caches at the same time listen to changes and fire a callback to update in-memory cache"

---
+++ src/cacheManager.js
@@ -0,0 +1,43 @@
+/*
+ helper class that will manage cache state
+   - commuincation between nodes when a cache changes
+   - listen to change events and delegate to Cache class to update in-memory cache
+*/
+
+const connect = require('./connect.js');
+const QUEUE = 'lur-cache';
+
+class CacheManger {
+
+  constructor() {
+    connect(this.recieve).then(data=>{
+      this.channel = data.channel;
+    });
+  }
+  reconnect() {
+    console.log('reconnecting...')
+    return new Promise((resolve, reject)=>{
+      connect().then(data=>{
+        this.channel = data.channel;
+        resolve()
+      });
+    })
+  }
+  publish(msg) {
+    // send message to queue
+    msg = JSON.stringify(msg);
+    if (!this.channel) {
+      return reconnect().then(()=>{
+        this.channel.sendToQueue(QUEUE, Buffer.from(msg));
+        console.log(" [x] Sent %s", msg);
+      });
+    }
+    this.channel.sendToQueue(QUEUE, Buffer.from(msg));
+    console.log(" [x] Sent %s", msg);
+  }
+  recieve(data) {
+    console.log('the data ', data.content.toString())
+  }
+}
+
+module.exports = CacheManger
