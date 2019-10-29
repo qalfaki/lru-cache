a11fd8248ecc0772581bb45de8bc159fcf5b1ee5
syntaxsizer <qusai.alfaki@gmail.com>
2019-10-29 18:55:44 +0300

    fix js context bug "this" was changing since the function is a callbac that makes a call to a bound function

--- src/cacheManager.js
+++ src/cacheManager.js
@@ -3,14 +3,14 @@
    - commuincation between nodes when a cache changes
    - listen to change events and delegate to Cache class to update in-memory cache
 */
-
+let c = 1;
 const connect = require('./connect.js');
 
 class CacheManger {
 
   constructor(url) {
       this.url = url;
-      this.startConnection(url, this.recieve)
+      this.startConnection(url, this.recieve.bind(this))
   }
   startConnection(url, cb) {
     console.log('connecting...')
@@ -29,15 +29,22 @@ class CacheManger {
       this.channel.sendToQueue(this.queue, Buffer.from(msg));
     }
     catch (err) {
-      this.startConnection(this.url, this.recieve).then(d=>{
+      this.startConnection(this.url, this.recieve.bind(this)).then(d=>{
         d.c.sendToQueue(d.q, Buffer.from(msg));
         console.log(" [x] Sent %s", msg);
       });
     }
-    console.log(" [x] Sent %s", msg);
   }
-  recieve(data) {
-    console.log('the data ', data.content.toString())
+  recieve(msg) {
+    console.log('this. channel ', this.channel);
+    msg = JSON.parse(msg.content.toString())
+    let action = msg.action;
+    let data = msg.data;
+    console.log('the counter ', c)
+    if (c !== 4) {
+      c ++
+      this[action](data.key, data.value, data.maxAge);
+    }
   }
 }
 
