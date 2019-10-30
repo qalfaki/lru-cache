4d96075fb98ebf833305925798de361c4803ac75
syntaxsizer <qusai.alfaki@gmail.com>
2019-10-31 00:42:37 +0300

    refactor cache class and simplify logic

--- src/cache.js
+++ src/cache.js
@@ -2,44 +2,28 @@ const CacheManger = require('./cacheManager.js');
 
 class Cache extends CacheManger {
 
-  constructor(limit=Infinity, maxAge=Infinity, stale=false, hashMap={}) {
+  constructor(maxAge=Infinity) {
     super()
-    this.size = 0;
-    this.limit = limit;
     this.maxAge = maxAge;
-    this.stale = stale;
-    this.hashMap = hashMap;
+    this.hashMap = {};
     this.head = null;
     this.tail = null;
+    // clear the cache once its expires
+    setTimeout(()=>this.reset(), this.maxAge === Infinity ? 0: this.maxAge);
   }
 
-  setHead(node) {
-    node.next = this.head;
-    node.previous = null;
-    if (this.head !== null)
-      this.head.previous = node;
-      this.head = node;
-      if (this.tail === null) {
+  set(key, value) {
+    const node = new Node(key, value);
+    if (Object.keys(this.hashMap).length) {
+        this.tail.next = node;
+        node.previous = this.tail;
         this.tail = node;
-      }
-      this.size += 1;
-      this.hashMap[node.data.key] = node;
-  }
-
-  setCache(cache) {
-    //TODO: implement
-  }
-
-  set(key, value, maxAge) {
-    maxAge = parseInt(maxAge) ? maxAge : this.maxAge;
-    const node = new Node(key, value, maxAge, Date.now() + maxAge);
-    if (this.size >= this.limit) {
-        delete this.hashMap[this.tail.data.key];
-        this.size -= 1;
-        this.tail = this.tail.previous;
-        this.tail.next = null;
     }
-    this.setHead(node);
+    else {
+      this.head = node;
+      this.tail = node;
+    }
+    this.hashMap[node.data.key] = node;
     this.publish({action: 'set', data: {key, value}})
   }
 
@@ -56,24 +40,14 @@ class Cache extends CacheManger {
       this.tail = node.previous;
     }
     delete this.hashMap[key];
-    this.size -= 1;
-    this.publish({action: 'remove', data: {key}})
   }
 
   get(key) {
     const oldNode = this.hashMap[key];
     if (oldNode) {
-      const value = oldNode.getValue();
-      const nodeMaxAge = oldNode.getMaxAge();
-      const maxAge = parseInt(nodeMaxAge) ? nodeMaxAge : this.maxAge;
-      if (Date.now() >= oldNode.getExpiry()) {
-        this.remove(key);
-        return this.stale ? oldNode.getValue() : null;
-      }
-      const newNode = new Node(key, value, maxAge, Date.now() + maxAge);
       this.remove(key);
-      this.setHead(newNode);
-      return value
+      this.set(oldNode.getKey(), oldNode.getValue());
+      return oldNode.value
     }
     return null
   }
@@ -83,11 +57,9 @@ class Cache extends CacheManger {
   }
 
   reset() {
-    this.size = 0;
     this.hashMap = {};
     this.head = null;
     this.tail = null;
-    this.publish({action: 'reset'})
   }
 
   toArray() {
@@ -116,42 +88,23 @@ class Cache extends CacheManger {
       node = node.next;
     }
   }
-
-  getSize() {
-    return this.size
-  }
-
-  delete(key) {
-    this.remove(key)
-  }
 }
 
 
 // double linked list
-
 class Node {
 
-  constructor(key, value, maxAge=Infinity, expires=Infinity) {
+  constructor(key, value) {
     if ([key, value].includes(undefined))
       throw new Error(`${[key, value].indexOf(undefined) == 0 ? 'key': 'value'} not provided`);
 
     this.data = {key, value};
     this.previous = null;
     this.next = null;
-    this.maxAge = maxAge;
-    this.expires = Infinity;
   }
 
   getValue() {
     return this.data.value;
-  }
-
-  getMaxAge() {
-    return this.maxAge;
-  }
-
-  getExpiry() {
-    return this.expires;
   }
 
   getKey() {
